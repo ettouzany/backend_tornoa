5428d82b94e974007a980a76bf96da3e3bd35094
ettouzany <ettouzany>
2020-12-11 17:03:30 +0100

    auth

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,11 +1,11 @@
 import {TypeOrmModuleOptions} from '@nestjs/typeorm';
 export const typeOrmConfig:TypeOrmModuleOptions ={
     type:'postgres',
-    host:'ec2-52-31-94-195.eu-west-1.compute.amazonaws.com',
+    host:'ec2-23-23-88-216.compute-1.amazonaws.com',
     port:5432,
-    username:'dxrdlgtqxtixbx',
-    password:'7a3ef1e77122582c79960920d8b375712981de891be6acb5561ad33471953462',
-    database:'d1mja0cjft30k7',
+    username:'ojdhjrosxhedns',
+    password:'59ac9676ea0cf7b6e3b1f7d68e131c65049b59d31f91801da84bfc9628414fa5',
+    database:'dedunskuvru9nm',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true
 }
